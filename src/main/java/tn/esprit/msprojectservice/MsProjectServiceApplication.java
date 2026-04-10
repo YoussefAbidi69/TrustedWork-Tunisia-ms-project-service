df9a249d2f4b5985791517caf792411d8e2ff373
YoussefAbidi69 <Youssef.abidi@esprit.tn>
2026-04-10 21:28:08 +0100

    Initial

---
+++ src/main/java/tn/esprit/msprojectservice/MsProjectServiceApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.msprojectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProjectServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsProjectServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/msprojectservice/MsProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.msprojectservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProjectServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
